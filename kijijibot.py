324a21c3afd235c63168a3c9f33ee5b085fd80ab
hacker-pyrat <parent.j.f@gmail.com>
2014-07-01 02:56:17 -0400

    fix

--- kijijibot.py
+++ kijijibot.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 #! -*- coding: utf-8 -*-
 
+from time import sleep
+import codecs
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -15,27 +17,28 @@ except ImportError:
 xpath = {}
 xpath['ads_page_btn'] = "//a[contains(text(), 'Mon Kijiji')]"
 xpath['login_page_btn'] = "//a[contains(text(), 'Ouvrir une session')]"
-xpath['email_input'] = "//input[@class = 'kdd_email']"
-xpath['pw_input'] = "//input[@name = 'Password']"
-xpath['login_btn'] = "//input[@id = 'signinbtn']"
+xpath['email_input'] = "//input[@id = 'LoginEmailOrNickname']"
+xpath['pw_input'] = "//input[@name = 'password']"
+xpath['login_btn'] = "//button[@id = 'SignInButton']"
 xpath['ads_category_btn'] = "//a[contains(text(), 'vente, vente de détail')]"
 xpath['post_ads_btn'] = "//input[@id = 'PostAd']"
 xpath['post_ads_page_btn'] = "//a[contains(@class, 'button-open')]"
-xpath['ad_title'] = "//input[@name = 'Title']"
-xpath['ad_desc'] = "//textarea[@name = 'Description']"
-xpath['ad_address'] = "//input[@name = 'MapAddress']"
+xpath['ad_title'] = "//input[@id = 'postad-title']"
+xpath['ad_desc'] = "//textarea[@id = 'pstad-descrptn']"
+xpath['ad_address'] = "//input[@id = 'pstad-map-address']"
 xpath['videos_input'] = "//input[@name = 'AdVideoUrl']"
-xpath['ad_delete_btn'] = "//a[@title = 'Supprimer']"
+xpath['ad_delete_btn'] = "//a[@class = 'cta-delete']"
 
 class Kijijibot():
 
     def __init__(self, args):
         
-        self.base_url = 'http://www.kijiji.ca/h-grand-montreal/80002'
+        self.base_url = 'http://www.kijiji.ca/h-longueuil-rive-sud/1700279'
         self.config = yaml.load(open("./config/config.yaml", 'r'))
         self.args = args
         self.ads = self.config.get('ads')
         print 'ads', self.ads
+        print open(self.ads[0].get('desc'), 'r').read()
 
         if self.config.get('dev'):
             self.driver = webdriver.Chrome()
@@ -91,7 +94,7 @@ class Kijijibot():
         self.find_and_click(xpath['login_btn'])
 
     def go_to_the_ads_page(self):
-        self.driver.get("http://montreal.kijiji.ca/c-SelectCategory")
+        self.driver.get("http://www.kijiji.ca/p-select-category.html")
 
         WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH,xpath['ads_category_btn'])))
 
@@ -102,17 +105,18 @@ class Kijijibot():
 
         self.find_and_click(xpath['ads_page_btn'])
 
-        WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH,"//div[contains(text(), 'Mes annonces')]")))
+        WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH,"//*[contains(text(), 'Mes annonces')]")))
 
         del_btns = self.driver.find_elements_by_xpath(xpath['ad_delete_btn'])
 
         for del_btn in del_btns:
-            self.driver.find_element_by_id(del_btn.get_attribute('id')).click()
+            self.driver.find_elements_by_xpath(xpath['ad_delete_btn'])[0].click()
 
             WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH,"//div[contains(text(), 'Supprimer l')]")))
 
-            self.driver.execute_script("document.getElementsByName('SurveyResponse')[3].checked = true")
-            self.driver.execute_script("document.getElementById('ManageMyAdsDeleteForm').submit()")
+            self.driver.execute_script('return document.querySelectorAll(\'[value = "PREFER_NOT_TO_SAY"]\');')[0].click()
+            #self.driver.execute_script("document.getElementById('DeleteModalSurveyForm').submit()")
+            self.driver.find_element_by_xpath('//input[@id = "DeleteSurveyOK"]').click()
 
     def go_to_home(self):
         self.driver.get(self.base_url)
@@ -130,31 +134,38 @@ class Kijijibot():
 
             self.find_and_click(xpath['ads_category_btn'])
 
-            WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH,"//span[contains(text(), 'Détails de l')]")))
-
-            #Ad type
-            self.driver.execute_script("document.getElementById('A_JobType_ID').options[1].selected = true")
+            WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH,xpath['ad_title'])))
 
             #Ad title
             self.find_and_sendkeys(xpath['ad_title'], ad.get('title'))
 
             #Ad desc
-            self.find_and_sendkeys(xpath['ad_desc'], ad.get('desc'))
+            desc = codecs.open(ad.get('desc'), encoding='utf-8').read()
+            self.find_and_sendkeys(xpath['ad_desc'], desc)
 
             #Ad subarea
-            self.driver.execute_script("document.getElementById('SubArea').options[4].selected = true")
+            #self.driver.execute_script("document.getElementById('SubArea').options[4].selected = true")
 
             #Ad address
             self.find_and_sendkeys(xpath['ad_address'], ad.get('address'))
 
-            #Terms of use
-            self.driver.execute_script("document.getElementById('TermsCheck').checked = true")
 
             #Videos
-            self.driver.find_element_by_xpath(xpath['videos_input']).clear()
+            #self.driver.find_element_by_xpath(xpath['videos_input']).clear()
 
             #Post
-            self.driver.execute_script("document.getElementById('MainForm').submit()")
+            self.driver.execute_script("document.getElementById('PostAdMainForm').submit()")
+
+            sleep(5)
+
+            #Terms of use
+            self.driver.execute_script("document.getElementById('PostAdConfirmationOfTerms').checked = true")
+
+            #Ad type
+            self.driver.execute_script("document.getElementById('jobtype_s').options[1].selected = true")
+
+            self.driver.execute_script("document.getElementById('PostAdMainForm').submit()")
+
         
 
     def logout(self):
